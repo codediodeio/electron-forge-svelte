4d97fedc07f6bbb64abadc976f5b97c75621697f
unknown <hello@jeffdelaney.me>
2020-02-29 10:10:29 -0700

    use nodeIntegration by default

--- src/index.js
+++ src/index.js
@@ -17,7 +17,10 @@ const createWindow = () => {
   // Create the browser window.
   const mainWindow = new BrowserWindow({
     width: 800,
-    height: 600
+    height: 600,
+    webPreferences: {
+      nodeIntegration: true
+    }
   });
 
   // and load the index.html of the app.
